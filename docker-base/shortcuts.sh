8337664d51c6ea8916c01659dfd2a18a56f5be6a
Ubuntu <ubuntu@ip-172-31-11-69.us-west-2.compute.internal>
2018-01-16 08:44:14 +0000

    New and improved docker helper commands
    No longer creates a new container everytime docker-run is called which wastes ALOT of diskspace and memory

--- docker-base/shortcuts.sh
+++ docker-base/shortcuts.sh
@@ -2,7 +2,14 @@
 
 docker-build () {
 	sudo docker build -t $1 .
+	sudo docker run --runtime=nvidia -it -d --name=$1 $1 > /dev/null
 }
 docker-run () {
-	sudo docker run --runtime=nvidia -it $1 bash
+	sudo docker start $1 > /dev/null
+	sudo docker exec -it $1 bash
+}
+docker-clean() {
+	sudo docker stop $(sudo docker ps -a -q)
+	sudo docker rm $(sudo docker ps -a -q)
+	sudo docker rmi -f $(sudo docker images -q)
 }
