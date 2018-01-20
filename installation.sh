c43150613505b1706f472b247a1e677b6dfeef59
root <root@ip-172-31-8-212.us-west-2.compute.internal>
2018-01-20 23:26:48 +0000

    Extra helpful installation

---
+++ installation.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y git
+sudo git clone https://github.com/callaunchpad/ml-docker.git /tmp/ml-docker
+sudo cp -rva /home/ubuntu/.ssh /tmp/ml-docker/
+sudo rm -r /home/ubuntu
+sudo mv /tmp/ml-docker /home/ubuntu
+
