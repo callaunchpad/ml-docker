e418188a6d4a506bded23e2bf3bfe508b1d318cc
Ubuntu <ubuntu@ip-172-31-8-212.us-west-2.compute.internal>
2018-01-21 03:36:47 +0000

    Permissions

--- installation.sh
+++ installation.sh
@@ -3,4 +3,4 @@ sudo git clone https://github.com/callaunchpad/ml-docker.git /tmp/ml-docker
 sudo cp -rva /home/ubuntu/.ssh /tmp/ml-docker/
 sudo rm -r /home/ubuntu
 sudo mv /tmp/ml-docker /home/ubuntu
-
+sudo chown -R ubuntu /home/ubuntu/
